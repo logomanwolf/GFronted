7b6f12bcad734c7d684dc92a4bd784473f7457a7
logomanwolf <1293789686@qq.com>
2019-07-08 10:27:10 +0800

    before npm run eject commit

--- src/component/canvas.jsx
+++ src/component/canvas.jsx
@@ -3,7 +3,8 @@ import { Card } from 'antd';
 const Canvas = () => {
     return (
         <div>
-            <Card  bordered={true} style={{ background:'#ECECEC',margin:10}} size={"small"} className="canvas">
+            <Card bordered={true} style={{ background: '#ECECEC', margin: 10 }} size={"small"} className="canvas">
+                
             </Card>
         </div>
     );
---
+++ src/component/forceGraph.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+// import {} from 'd3';
+const ForceGraph = () => {
+    return ( 
+
+
+     );
+}
+ 
+export default ForceGraph;
