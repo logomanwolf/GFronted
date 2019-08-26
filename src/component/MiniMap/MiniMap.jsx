6e61d1ed9b2fe9b6361a93874d232dc457476da5
logomanwolf <1293789686@qq.com>
2019-08-26 14:20:45 +0800

    stable

--- src/component/MiniMap/MiniMap.jsx
+++ src/component/MiniMap/MiniMap.jsx
@@ -15,11 +15,17 @@ class MiniMap extends Component{
 		this.canvasWidth = 1380;
 		this.canvasHeight = 1000;
 		this.state = {
-			svgStyle: {width: '100%',height:'100%'}
+			svgStyle: { width: '100%', height: '100%' },
+			timeStamp: new Date()
 		}
 	}
 
-
+	zoomed = () => {
+		// contourG.select('rect').attr('transform', d3.event.transform);
+		let svgStyle = { ...this.state.svgStyle };
+		this.setState({ svgStyle, timeStamp: new Date() });
+		// contourG.selectAll('path.contour')
+	}
     drawContour() {
 		console.log("drawContour")
 		let contourFunc = contour.contourDensity()
@@ -76,26 +82,34 @@ class MiniMap extends Component{
 				color: colorBlue(d.value)
 			}
 		})
+		let zoom = d3.zoom().on('zoom', this.zoomed);
+		
 		// console.log(data)
 		let contourG = d3.select("#minimap-wrapper").select("svg")
+			.call(zoom)
 		
 		let contourMap = contourG.selectAll("path.contour")
-			.data(data, (d, i) => i)
-		let zoomed = () => {
-			contourG.select('rect').attr('transform', d3.event.transform);
+			.data(data, (d, i) => i);
+		contourG.on('drag', (d) => {
+			d3.select(this).attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
+			console.log(d3.event.x);
+		});
 
-			// contourG.selectAll('path.contour')
-		}
-		let zoom = d3.zoom().scaleExtent([0.5, 10]).on('zoom', zoomed);
+		// let zoom = d3.zoom().scaleExtent([0.5, 10]).on('zoom', this.zoomed);
 		contourMap.enter()
 		.append("path")
 		.classed('contour', true)
+		
 
 		contourMap.exit().remove()
 
 		contourG.selectAll("path.contour")
 			.attr("d", d => d.path)
 			.attr("fill", d => d.color)
+			// .on('drag', (d) => {
+			// 	d3.select(this).attr("x", d.x = d3.event.x).attr("y", d.y = d3.event.y);
+			// 	console.log(d3.event.x);
+			// });
 
 		console.log(contourMap)
 		
@@ -110,11 +124,11 @@ class MiniMap extends Component{
 			.attr('stroke', '#999')
 			.attr('stroke-width', 2)
 			.attr('fill-opacity', 0)
-			.call(zoom);
+			
     }
 	
 	calContour(){
-		let xCoor=[],yCoor=[]
+		let xCoor = [], yCoor = [], transform = { k: 1, x: 0, y: 0 };
 		for(let i=0;i<this.nodes.length;i++){
 			xCoor[i]=this.nodes[i].attrs['x']
 			yCoor[i]=this.nodes[i].attrs['y']
@@ -122,20 +136,41 @@ class MiniMap extends Component{
 		// console.log(xCoor)
 		var xMax=Math.max.apply(null,xCoor)
 		var yMax=Math.max.apply(null,yCoor)
-		console.log(xMax,yMax)
-		for(let i=0;i<this.nodes.length;i++){
-			this.nodes[i].x=this.nodes[i].attrs.x*this.svgWidth/xMax*0.8+20
-			this.nodes[i].y = this.nodes[i].attrs.y * this.svgHeight / yMax * 0.8 + 10
-			this.nodes[i].id = this.nodes[i].id;
+		console.log(xMax, yMax)
+		if (d3.event !== undefined && d3.event!==null)
+		{
+			transform = d3.event.transform;
+			console.log(transform);
 		}
+		this.graph.transform = transform;
+		for(let i=0;i<this.nodes.length;i++){
+			this.nodes[i].attrs.x= this.nodes[i].attrs.x * transform.k+ transform.x;
+			this.nodes[i].attrs.y  = this.nodes[i].attrs.y * transform.k  + transform.y;
+		}
+		// for(let i=0;i<this.nodes.length;i++){
+		// 	this.nodes[i].x = (this.nodes[i].attrs.x*transform.k * this.svgWidth / xMax ) + transform.x;
+		// 	this.nodes[i].y = (this.nodes[i].attrs.y* transform.k * this.svgHeight / yMax)  + transform.y;
+		// 	this.nodes[i].id = this.nodes[i].id;
+		// }
+		
+		for(let i=0;i<this.nodes.length;i++){
+			this.nodes[i].x = this.nodes[i].attrs.x* this.svgWidth / xMax *0.8
+			this.nodes[i].y = this.nodes[i].attrs.y* this.svgHeight / yMax *0.8 
+		}
+		this.graph.draw();
 	}
-	
-	shouldComponentUpdate(nextProps) {
-        if (nextProps.graph !== undefined && this.graph!==nextProps.graph) {
-			this.graph = nextProps.graph
-			this.nodes = this.graph.nodes().toArray();
+	updateNodes(g) {
+		this.graph = g;
+		this.nodes = this.graph.nodes().toArray();
+	}
+	shouldComponentUpdate(nextProps,nextState ) {
+		if ((nextProps.graph !== undefined && this.graph !== nextProps.graph) ) {
+			this.updateNodes(nextProps.graph)
 			console.log("minimap should update", nextProps, this)
             return true
+		}
+		else if (nextState.timeStamp !== this.state.timeStamp) {
+			return true;
 		}
 		else {
             console.log("minimap shouldn't update", nextProps, this)
