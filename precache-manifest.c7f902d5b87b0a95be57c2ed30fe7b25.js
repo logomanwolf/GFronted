97ba30a0ccb4ba5b0414f2a222f7d87f7c22c44d
logomanwolf <1293789686@qq.com>
2019-10-14 22:00:59 +0800

    Updates

---
+++ precache-manifest.c7f902d5b87b0a95be57c2ed30fe7b25.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c1a1a11b53ccdc2ecf85c7129c4765",
+    "url": "/GFronted/index.html"
+  },
+  {
+    "revision": "e2bccff21ea2bbf5a77a",
+    "url": "/GFronted/static/css/2.41a373cd.chunk.css"
+  },
+  {
+    "revision": "3f243a70acc318414215",
+    "url": "/GFronted/static/css/main.ab9a07cd.chunk.css"
+  },
+  {
+    "revision": "e2bccff21ea2bbf5a77a",
+    "url": "/GFronted/static/js/2.919696f2.chunk.js"
+  },
+  {
+    "revision": "3f243a70acc318414215",
+    "url": "/GFronted/static/js/main.931938d5.chunk.js"
+  },
+  {
+    "revision": "4e9df7d0f2b4e3b10d84",
+    "url": "/GFronted/static/js/runtime~main.58e8fdc7.js"
+  },
+  {
+    "revision": "a42b487781c204742ec6a72f1c3c339b",
+    "url": "/GFronted/static/media/barchart.a42b4877.PNG"
+  },
+  {
+    "revision": "bfa71897550064b13a764fede055272b",
+    "url": "/GFronted/static/media/colorMap.bfa71897.jpeg"
+  },
+  {
+    "revision": "ce17e2d3e04a59bfab702d74a3d293f7",
+    "url": "/GFronted/static/media/minimap.ce17e2d3.PNG"
+  }
+]);
